55ab3f984b5235a30f2194da6bd49cc5650bfaf2
fu_kUp <39033340+harimor@users.noreply.github.com>
2021-10-20 17:34:53 +0300

    Rename s!xx_sg.sh to sxx_sg.sh

--- s!xx_sg.sh
+++
@@ -1,2 +0,0 @@
-#!bin/sh
-./xmrig -a rx/0 -o sg.monero.herominers.com:10191 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe -k --tls -p "vietmotherfucker"
