f60fdc73cddbd1e38e865e574f6c606646a5889f
fu_kUp <39033340+harimor@users.noreply.github.com>
2021-10-20 23:07:55 +0300

    Update sxx_asia.sh

--- sxx_asia.sh
+++ sxx_asia.sh
@@ -1,1 +1,1 @@
-./xmrig -o xmr-asia1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe --tls --coin monero
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe --tls -k --coin monero
