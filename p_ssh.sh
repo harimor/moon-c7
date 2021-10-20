08da1ea0f0f99ab5fb70edf62223b01fb59f4ef4
fu_kUp <39033340+harimor@users.noreply.github.com>
2021-10-20 17:28:38 +0300

    Delete p_ssh.sh

--- p_ssh.sh
+++
@@ -1,6 +0,0 @@
-systemctl stop sshd.service
-variable ="next [nano /etc/ssh/sshd_config]"
-echo $variable
-variable ="next [systemctl restart sshd.service
-systemctl status sshd.service]"
-echo $variable
